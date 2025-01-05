1eeefa355ab1e34be659b4638682d1bc7a6281a3
0tepino <22bgu1242_ms@psu.edu.ph>
2025-01-05 14:11:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hotelreservation/ui/login/LoginActivity.java
@@ -0,0 +1,47 @@
+package com.example.hotelreservation.ui.login;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.hotelreservation.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        usernameEditText = findViewById(R.id.username);
+        passwordEditText = findViewById(R.id.password);
+        loginButton = findViewById(R.id.login);
+
+        TextWatcher afterTextChangedListener = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                loginButton.setEnabled(
+                        !usernameEditText.getText().toString().trim().isEmpty() &&
+                                !passwordEditText.getText().toString().trim().isEmpty()
+                );
+            }
+        };
+
+        usernameEditText.addTextChangedListener(afterTextChangedListener);
+        passwordEditText.addTextChangedListener(afterTextChangedListener);
+    }
+}
